9ed76c80a26dd97e3eca5337e27fdc48d409e052
Aziz-Rashid <axixraxhid@gmail.com>
2021-12-09 18:23:43 +0500

    update

--- src/pages/contactus.js
+++ src/pages/contactus.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Layout from '../components/Layout/Layout'
-import {Button} from 'antd'
+import { notification } from 'antd'
 import './index.css'
 const ContactUs = () => {
+    const [userInfo, setUserInfo] = useState({
+        name: "",
+        email: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+        phone: ""
+    })
+    const openNotificationWithIcon = type => {
+        notification[type]({
+            message: 'Gemini Notification!',
+            description:
+                'You have Successfully submit your information Thanks you!',
+        });
+    };
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+    const handleOnchange = e => {
+        setUserInfo({
+            ...userInfo,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...userInfo })
+        })
+            .then(() => {
+                openNotificationWithIcon('success')
+                setUserInfo({
+                    name: "",
+                    email: "",
+                    address: "",
+                    city: "",
+                    state: "",
+                    zip: "",
+                    phone: ""
+                })
+            })
+            .catch(error => alert(error));
+
+    };
     return (
         <Layout>
             <div >
@@ -12,22 +62,25 @@ const ContactUs = () => {
                 <div className="flex_contact">
                     <div className="aka">
                         <div className="flex2s">
-                                <h1 className="wes">How Can We Help You With Your HVAC Needs?</h1>
-                                <p className="nam">Name*</p>
-                                <input className="inp" type="text" />
-                                <p className="nam">Email*</p>
-                                <input className="inp" type="text" />
-                                <p className="nam">Address*</p>
-                                <input className="inp" type="text" />
-                                <p className="nam">City*</p>
-                                <input className="inp" type="text" />
-                                <p className="nam">State*</p>
-                                <input className="inp" type="text" />
-                                <p className="nam">Zip*</p>
-                                <input className="inp" type="text" />
-                                <p className="nam">Phone*</p>
-                                <input className="inp" type="number" />
-                                <button className="contactUsbtn">Click To Contact US</button>
+                            <h1 className="wes">How Can We Help You With Your HVAC Needs?</h1>
+                            <form name="contact" method="post" onSubmit={(e) => handleSubmit(e)} data-netlify="true" data-netlify-honeypot="bot-field">
+                                <input type="hidden" name="form-name" value="contact" />
+                                <label htmlFor="name" className="nam">Name*</label>
+                                <input name="name" value={userInfo.name} onChange={(e) => handleOnchange(e)} placeholder="Type your Full Name..." className="inp" type="text" />
+                                <label htmlFor="email" className="nam">Email*</label>
+                                <input name="email" value={userInfo.email} onChange={(e) => handleOnchange(e)} placeholder="Type your Email..." className="inp" type="text" />
+                                <label htmlFor="address" className="nam">Address*</label>
+                                <input name="address" value={userInfo.address} onChange={(e) => handleOnchange(e)} placeholder="Type your Address..." className="inp" type="text" />
+                                <label htmlFor="city" className="nam">City*</label>
+                                <input name="city" value={userInfo.city} onChange={(e) => handleOnchange(e)} placeholder="Type your City..." className="inp" type="text" />
+                                <label htmlFor="state" className="nam">State*</label>
+                                <input name="state" value={userInfo.state} onChange={(e) => handleOnchange(e)} placeholder="Type your State..." className="inp" type="text" />
+                                <label htmlFor="zip" className="nam">Zip*</label>
+                                <input name="zip" value={userInfo.zip} onChange={(e) => handleOnchange(e)} placeholder="Type your Zip..." className="inp" type="text" />
+                                <label htmlFor="phone" className="nam">Phone*</label>
+                                <input name="phone" value={userInfo.phone} onChange={(e) => handleOnchange(e)} placeholder="Type your Phone..." className="inp" type="number" />
+                                <button type="submit" className="contactUsbtn">Click To Contact US</button>
+                            </form>
                         </div>
                     </div>
                     <div className="wid">
