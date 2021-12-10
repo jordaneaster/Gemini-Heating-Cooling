74e18fd7a1b12c3cff0bdd3f7bdafa8023ba37ab
jordaneaster <jordane@am-collab.com>
2021-12-10 14:26:25 -0500

    site logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/gemini.png",
       },
     },
     {
